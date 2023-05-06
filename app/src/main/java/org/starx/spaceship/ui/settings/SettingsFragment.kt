0a362dff9a59c8822ce52841fe83681c031d1548
Suzukihonoka <sakurafirex@gmail.com>
2023-05-07 00:50:56 +0800

    chore: optimize imports

--- app/src/main/java/org/starx/spaceship/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/starx/spaceship/ui/settings/SettingsFragment.kt
@@ -5,7 +5,12 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
