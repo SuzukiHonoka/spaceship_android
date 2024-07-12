84c9376d419a2bd57e99070b76b1a61ebe8c0601
Suzukihonoka <sakurafirex@gmail.com>
2024-07-12 21:45:15 +0800

    chore: optimize imports

--- app/src/main/java/org/starx/spaceship/receiver/OnBootComplete.kt
+++ app/src/main/java/org/starx/spaceship/receiver/OnBootComplete.kt
@@ -3,7 +3,6 @@ package org.starx.spaceship.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import org.starx.spaceship.service.Background
