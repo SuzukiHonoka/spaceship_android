e9e0b1abf0e3f3e81182e79cdc201ae621bb04f4
SuzukiHonoka <sakurafirex@gmail.com>
2025-01-27 15:15:21 +0800

    cleanup: remove unnecessary annotation

--- app/src/main/java/org/starx/spaceship/util/JsonFactory.kt
+++ app/src/main/java/org/starx/spaceship/util/JsonFactory.kt
@@ -1,11 +1,9 @@
 package org.starx.spaceship.util
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
 class JsonFactory {
     companion object {
-        @OptIn(ExperimentalSerializationApi::class)
         val processor = Json {
             encodeDefaults = true
             explicitNulls = false
